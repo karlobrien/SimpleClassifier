0f7fabb886293a9d379af7b15d98165d12def30f
karlobrien <karlobrien@gmail.com>
2013-11-11 22:13:52 +0000

    Adding more features to average calculation

--- classifier.py
+++ classifier.py
@@ -7,33 +7,34 @@ class Features(object):
     def __init__(self, name):
         self.title = name
         self.workClass = defaultdict(int)
-        self.educationNumber = defaultdict(int)
         self.maritialStatus = defaultdict(int)
         self.occupation = defaultdict(int)
         self.relationship = defaultdict(int)
         self.race = defaultdict(int)
         self.sex = defaultdict(int)
-        self.capitalGain = defaultdict(int)
-        self.capitalLoss = defaultdict(int)
-        self.hoursPerWeek = defaultdict(int)
+
+        self.capitalGain = 0
+        self.averageCapitalGain = 0
+        self.capitalLoss = 0
+        self.averageCapitalLoss = 0
+        self.hoursPerWeek = 0
+        self.averageHoursPerWeek = 0
+
+        self.count = 0
 
         self.totalAge = 0
-        self.count = 0
         self.averageAge = 0
-        self.test = 0
+
+        self.totalEducation = 0
+        self.averageEducation = 0
 
     def assign(self, line):
-        self.age[line[0]] += 1
-        #self.workClass[line[1]] += 1
-        self.educationNumber[line[4]] += 1
+        self.workClass[line[1]] += 1
         self.maritialStatus[line[5]] += 1
         self.occupation[line[6]] += 1
         self.relationship[line[7]] += 1
         self.race[line[8]] += 1
         self.sex[line[9]] += 1
-        self.capitalGain[line[10]] += 1
-        self.capitalLoss[line[11]] += 1
-        self.hoursPerWeek[line[12]] += 1
 
     def incrementCount(self):
         self.count = self.count + 1
@@ -41,15 +42,32 @@ class Features(object):
     def getAverageAge(self):
         return self.totalAge / self.count
 
-    def averageValue(self, line):
-        self.totalAge += int(line[0])
+    def getAverageEducationClass(self):
+        return self.totalEducation / self.count
+
+    def getAverageCapitalGain(self):
+        return self.capitalGain / self.count
+
+    def getAverageCapitalLoss(self):
+        return self.capitalLoss / self.count
+
+    def getAverageHoursPerWeek(self):
+        return self.hoursPerWeek / self.count
 
     def averageWorkClass(self, line):
         self.workClass[line[1]] += 1
 
+    def assignTotals(self, line):
+        self.totalAge += int(line[0])
+        self.totalEducation += int(line[4])
+        self.capitalGain += int(line[10])
+        self.capitalLoss += int(line[11])
+        self.hoursPerWeek += int(line[12])
+
     def takeFeatures(self, line):
         self.incrementCount()
-        self.averageValue(line)
+        self.assign(line)
+        self.assignTotals(line)
 
 def main():
     below50k = Features("below50k")
@@ -65,14 +83,13 @@ def main():
                 above50k.takeFeatures(feature)
 
     print below50k.title
-    print below50k.totalAge
-    print below50k.count
-    print below50k.getAverageAge()
+    print "Average Age: " ,below50k.getAverageAge()
+    print "Average Work Class: " ,below50k.getAverageEducationClass()
+
 
     print above50k.title
-    print above50k.totalAge
-    print above50k.count
-    print above50k.getAverageAge()
+    print "Average Age: " ,above50k.getAverageAge()
+    print "Average Work Class: " ,above50k.getAverageEducationClass()
 
 if __name__ == '__main__':
     main()
